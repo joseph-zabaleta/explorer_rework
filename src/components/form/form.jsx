c749e46ff7060fc4dd2fc8a34e0f34b554981ec7
joseph-zabaleta <joseph.l.zabaetal@gmail.com>
2020-12-06 21:38:58 -0800

    working on form and callbacks

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './form.scss';
 
 export default function Form() {
 
+    const [query, setQuery] = useState('');
+
+    const handleChange = (event) => {
+
+        setQuery(event.target.value);
+
+    }
+
+    function handleSubmit (event) {
+
+        event.preventDefault();
+        // query location service get lon and lat to pass to other services
+        // const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+
+    }
+
     return (
-        <form>
+        <form onSubmit={ handleSubmit }>
             <label>Search your city: </label>
-            <input type="text" id="searchQuery" name="searchQuery" />
+            <input type="text" id="searchQuery" name="searchQuery" onChange={handleChange} />
             <button type="submit"> Search </button>
         </form>
     )
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import Form from '../form/form.jsx';
 import Location from '../location/location.jsx';
 import './main.scss';
 
-export default function Main(){
+export default function Main() {
+
+
 
     return (
         <main>
